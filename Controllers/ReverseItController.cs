226081e8213dc923977c0d6f717ff37b1f7ce4ea
JeremyLapham <jeremylapham2004@gmail.com>
2022-10-27 08:57:27 -0700

    Reverse It - Takes string and numbers

---
+++ Controllers/ReverseItController.cs
@@ -0,0 +1,27 @@
+//Jeremy Lapham
+//10-27-22
+//Reverse It - Endpoint
+//This program will take whatever the user has in the url and reverse it 
+//Peer Review: 
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaphamJReverseIt_EndPoint.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReverseItController : ControllerBase
+{
+    [HttpGet]
+    [Route("ReverseIt/{numOrString}")]
+
+    public string ReverseIt(string numOrString)
+    {
+        string text = "";
+        for (int i = numOrString.Length - 1; i >= 0; i--)
+        {
+            text = text + numOrString[i];
+        }
+        return $"Your Word/Number: {numOrString} |||| The Reverse: {text}";
+    }
+
+}
